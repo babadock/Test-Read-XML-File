8ce2a598f4dc6f125ff1d5486ad63800068b568f
babadock <cse.51104279@gmail.com>
2015-05-22 10:39:50 +0700

    Read image from XML file successfull.

--- jni/FileStorage-jni.cpp
+++ jni/FileStorage-jni.cpp
@@ -18,33 +18,6 @@ JNIEXPORT void JNICALL Java_com_example_testreadxmlfile_FileStorage_nativeCalcFe
 	LOGD("Entering nativeCalcFeatures !!!");
 	const char *nativeString = env->GetStringUTFChars(location, NULL);
 	string filepath = string(nativeString);
-
-	{
-		Mat R = Mat_<uchar>::eye(3, 3), T = Mat_<double>::zeros(3, 1);
-		int sz[3] = { 2, 2, 2 };
-		//Mat L(3, sz, CV_8UC(1), Scalar::all(0));
-		//Mat C = (Mat_<double>(3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
-
-		FileStorage fs(filepath, FileStorage::WRITE);
-		fs << "iterationNr" << 100;
-		fs << "strings" << "[";
-		fs << "image1.jpg" << "Awasomeness" << "baboon.jpg";
-		fs << "]";
-
-		fs << "Mapping";
-		fs << "{" << "One" << 1;
-		fs << "Two" << 2 << "}";
-
-		fs << "R" << R;
-		fs << "T" << T;
-
-		//fs << "C" << C;
-
-		LOGD("WRITE successful !!!");
-
-		fs.release();
-	}
-
 	{
 		FileStorage fs;
 		fs.open(filepath, FileStorage::READ);
@@ -52,37 +25,15 @@ JNIEXPORT void JNICALL Java_com_example_testreadxmlfile_FileStorage_nativeCalcFe
 		if (!fs.isOpened()) {
 			LOGI("Can't open file: %s ", filepath.c_str());
 		}
-		//LOGD("DEBUG Step 1");
 
-		FileNode n = fs["strings"];
+		Mat M;
 
-		//LOGI("DEBUG Step 2: %d", n.type());
+		LOGD("READ BEGING !!!");
+		fs["M"] >> M;
 
-		if (n.type() != FileNode::SEQ) {
-			LOGD("strings is not a sequence ! FAIL");
-		}
+		LOGI("%d , %d\n", M.rows, M.cols);
 
-		LOGI("DEBUG Step 3");
-
-		/*FileNodeIterator it = n.begin(), it_end = n.end();
-		 for (; it != it_end; it_end = n.end()) {
-		 //LOGI();
-		 }*/
-
-		n = fs["Mapping"];
-		LOGI("%d\n", (int )n["Two"]);
-		LOGI("%d\n", (int )n["One"]);
-
-		//LOGI("DEBUG Step 4");
-
-		Mat R;
-		fs["R"] >> R;
-
-		LOGI("%d , %d\n", R.rows, R.cols);
-
-		LOGI("DEBUG Step 5");
-
-		//LOGD("READ ENDED !!!");
+		LOGD("READ ENDED !!!");
 	}
 
 }
